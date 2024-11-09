2d56b9006bb90ef5ed6c2616d2d6ffdd0db469f8
xkc <xkylechan@gmail.com>
2024-11-08 23:28:15 -0700

    created function getComputerChoice.  Prints answer in console

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world!');
+function getComputerChoice() {
+  let number = Math.random();
+  if (number < 0.33) {
+    console.log('rock');
+    console.log(number);
+    return 'Rock';
+  } else if (number > 0.66) {
+    console.log('Paper');
+    console.log(number);
+    return 'Paper';
+  } else if (number > 0.33 && number < 0.66) {
+    console.log('Scissors');
+    console.log(number);
+    return 'Scissors';
+  }
+}
+
+getComputerChoice();

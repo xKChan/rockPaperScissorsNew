41a717c7efeefcbdb9081a32fcc8829f6c626bcf
xkc <xkylechan@gmail.com>
2024-11-11 23:44:51 -0700

    Button starts 1 round game with random computer pick and user is able to pick choice with buttons

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let playerPick = prompt('Rock, Paper, or Scissors? ');
-  let playerPickConverted =
-    playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase();
-  return playerPickConverted;
-}
+// function getHumanChoice() {
+//   let playerPick = prompt('Rock, Paper, or Scissors? ');
+//   let playerPickConverted =
+//     playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase();
+//   return playerPickConverted;
+// }
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
@@ -40,36 +40,62 @@ function playRound(humanChoice, computerChoice) {
   console.log('Score: Human: ' + humanScore + ' // Computer: ' + computerScore);
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  if (humanScore > computerScore) {
-    console.log(
-      'Congrats! You Win !! // Player Score: ' +
-        humanScore +
-        ' Computer Score: ' +
-        computerScore
-    );
-  } else if (humanScore < computerScore) {
-    console.log(
-      'Boo! You Lose !! // Player Score: ' +
-        humanScore +
-        ' Computer Score: ' +
-        computerScore
-    );
-  } else {
-    console.log(
-      'Tie Game! No one wins // Player Score: ' +
-        humanScore +
-        ' Computer Score: ' +
-        computerScore
-    );
-  }
-  return;
+function clickedRock() {
+  let computerSelection = getComputerChoice();
+  playRound('Rock', computerSelection);
+  console.log('I Picked Rock');
 }
 
-playGame();
+function clickedPaper() {
+  let computerSelection = getComputerChoice();
+  playRound('Paper', computerSelection);
+  console.log('I Picked Paper');
+}
+
+function clickedScissors() {
+  let computerSelection = getComputerChoice();
+  playRound('Scissors', computerSelection);
+  console.log('I Picked Scissors');
+}
+
+const pickedRock = document.getElementById('rock');
+const pickedPaper = document.getElementById('paper');
+const pickedScissors = document.getElementById('scissors');
+
+pickedRock.addEventListener('click', clickedRock);
+pickedPaper.addEventListener('click', clickedPaper);
+pickedScissors.addEventListener('click', clickedScissors);
+
+// function playGame() {
+//   for (let i = 0; i < 5; i++) {
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+//   if (humanScore > computerScore) {
+//     console.log(
+//       'Congrats! You Win !! // Player Score: ' +
+//         humanScore +
+//         ' Computer Score: ' +
+//         computerScore
+//     );
+//   } else if (humanScore < computerScore) {
+//     console.log(
+//       'Boo! You Lose !! // Player Score: ' +
+//         humanScore +
+//         ' Computer Score: ' +
+//         computerScore
+//     );
+//   } else {
+//     console.log(
+//       'Tie Game! No one wins // Player Score: ' +
+//         humanScore +
+//         ' Computer Score: ' +
+//         computerScore
+//     );
+//   }
+//   return;
+// }
+
+// playGame();
 // playRound(humanSelection, computerSelection);

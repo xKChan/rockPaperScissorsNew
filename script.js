f999620a005f99e4207b7c736db860a237648a1e
xkc <xkylechan@gmail.com>
2024-11-12 21:08:14 -0700

    1 round winner displays with DOM instead of console log now

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const pickedRock = document.getElementById('rock');
+const pickedPaper = document.getElementById('paper');
+const pickedScissors = document.getElementById('scissors');
+const score = document.getElementById('scoreBoard');
+
+pickedRock.addEventListener('click', clickedRock);
+pickedPaper.addEventListener('click', clickedPaper);
+pickedScissors.addEventListener('click', clickedScissors);
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -21,6 +30,7 @@ function getComputerChoice() {
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
+    getScore('Tie', humanChoice, computerChoice);
     console.log('Tie! No one wins.');
   } else if (
     (humanChoice == 'Rock' && computerChoice == 'Scissors') ||
@@ -29,6 +39,7 @@ function playRound(humanChoice, computerChoice) {
   ) {
     console.log('You Win! ' + humanChoice + ' beats ' + computerChoice);
     humanScore++;
+    getScore('Player', humanChoice, computerChoice);
   } else if (
     (humanChoice == 'Rock' && computerChoice == 'Paper') ||
     (humanChoice == 'Paper' && computerChoice == 'Scissors') ||
@@ -36,6 +47,7 @@ function playRound(humanChoice, computerChoice) {
   ) {
     console.log('You Lose! ' + humanChoice + ' loses to ' + computerChoice);
     computerScore++;
+    getScore('Computer', humanChoice, computerChoice);
   }
   console.log('Score: Human: ' + humanScore + ' // Computer: ' + computerScore);
 }
@@ -58,13 +70,18 @@ function clickedScissors() {
   console.log('I Picked Scissors');
 }
 
-const pickedRock = document.getElementById('rock');
-const pickedPaper = document.getElementById('paper');
-const pickedScissors = document.getElementById('scissors');
-
-pickedRock.addEventListener('click', clickedRock);
-pickedPaper.addEventListener('click', clickedPaper);
-pickedScissors.addEventListener('click', clickedScissors);
+function getScore(winner, humanPick, compPick) {
+  if (winner == 'Player') {
+    score.textContent = `You Win! ${humanPick} beats ${compPick}`;
+    score.appendChild;
+  } else if (winner == 'Computer') {
+    score.textContent = `You Lose! ${humanPick} loses to ${compPick}`;
+    score.appendChild;
+  } else {
+    score.textContent = `No one Wins ! It's a Tie. `;
+    score.appendChild;
+  }
+}
 
 // function playGame() {
 //   for (let i = 0; i < 5; i++) {
